82f5d7449872ac2fcc676423b31c2fe8ba4d9c71
Z-Kamili <zakariakamili97@gmail.com>
2020-03-20 20:19:02 +0100

    modification bien fait

--- Traitement/file.js
+++ Traitement/file.js
@@ -1,3 +1,8 @@
+//variable
+var select = document.getElementById("select");
+var url = "http://localhost:8000/Traitement/JSON/service.json";
+var request = new XMLHttpRequest();
+//Traitement animation
 $('.contact').click(()=>{
 $('.contact').addClass("active");
 $('.question').removeClass("active");
@@ -17,15 +22,34 @@ $('.question').addClass("active");
         
     },'slow');
 });
-/*traitement sur fichier json*/
-// const fs = require('fs');
-// var xmlhttp = new XMLHttpRequest();
-// xmlhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//         var myObj = JSON.parse(this.responseText);
-       
-//     }
-// };
-// xmlhttp.open("GET", "../JSON/service.json", true);
-// xmlhttp.send();
+var data_service = "";
+//Load data to object javascript using AJAX
+window.onload = function() {
+    request.open("GET", url);
+    request.onload = function() {
+        if (request.status == 200) {
+            data_service = JSON.parse(request.responseText);
+            RemplirSelect(data_service);
+        }
+    };
+    request.send(null);
+
+}
+
+//remplir select box using javascript 
+function RemplirSelect(data){
+
+    for(var  i = 0;i<data.length;i++){
+        var option = document.createElement("option");
+        option.innerText = data[i].Nom_service;
+        select.appendChild(option);
+    }
+}
+
+
+
+
+
+
+
 
--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 var express = require("express"),
 fs = require('fs'),
 app = express(),
+bodyParser = require('body-parser'),
 personne = "",
-http = require("http").Server(app).listen(80);
+http = require("http").Server(app).listen(8000);
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use("/css",express.static("./css"))
 app.use("/Traitement",express.static("./Traitement"))
 app.use("/img",express.static("./img"))
 app.get("/questionnement.html",function(req,res){ 
-fs.readFile('JSON/service.json', function(erreur, fichier) {
+fs.readFile('Traitement/JSON/service.json', function(erreur, fichier) {
     personne = JSON.parse(fichier);
-    console.log(personne);
 });
     res.sendFile(__dirname+"/questionnement.html");
 })
@@ -18,7 +19,8 @@ app.get("/Reclamation.html",function(req,res){
 
     res.sendFile(__dirname+"/Reclamation.html");
 });
+app.post("/questionnement.html",urlencodedParser,function(req,res){
+  console.log(req.body.service);
+  res.end("/questionnement.html");
+});
 
-exports.dataJSON = function(){
-    return personne;
-    }
